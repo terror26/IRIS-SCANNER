c0567c6a88c22d3d5da6bd78d723437872492517
Kanishk Verma <kanishkverma@Kanishks-MacBook-pro.local>
2019-04-09 17:02:23 +0530

    Beta third

---
+++ IITDatabase/Readme.txt
@@ -0,0 +1,37 @@
+Description of the IITD Iris Image Database version 1.0
+=======================================================
+
+This iris image database mainly consists of the iris images collected from the students and staff at IIT Delhi, India. This database has been acquired in
+the Biometrics Research Laboratory during January - July 2007 using JIRIS, JPC1000, digital CMOS camera. The acquired images were saved in bitmap format.
+The database of 2240 images is acquired from 224 different users and made available freely to the researchers. All the subjects in the database are in the
+age group 14-55 years comprising of 176 males and 48 females. The resolution of these images is 320 x 240 pixels and all these images were acquired in the
+indoor environment. All the images in the database were acquired from the volunteers who were not paid or provided any honorarium. The images were acquired
+using an automated program that requires users to present their eyes in a sequence until ten images are registered.
+
+Organization of Database
+========================
+The acquired database is saved in 224 folders, each corresponding to 224 subjects. Majority of images were acquired from the left eyes while the rest images
+were acquired from right eye. Now the database has a label 'L' or 'R' which designates left or right eye. There are 1288 images from 224 subject that are from
+left eyes while the rest images from 211 subjects are from right eyes.Except folders 1-13, 27, 55 and 65 all other folders have five left and 5 right eye 
+images. (**appended on 20-04-2016**).
+
+Usage of Database
+========================
+This database is only available for research and noncommercial purposes. Commercial distribution or any act related to commercial use of this database is strictly
+prohibited. Kindly acknowledge all the publicly available publications/work employing this database with the following acknowledgment:
+
+"Portions of the work tested on the IITD Iris Database version 1.0"
+A citation to "IIT Delhi Iris Database version 1.0, http://web.iitd.ac.in/~biometrics/Database_Iris.htm
+
+Related Publication:
+====================
+Ajay Kumar and Arun Passi, "Comparison and combination of iris matchers for reliable personal identification," Proc. CVPR 2008, Anchorage, Alaska, pp. 21-27 Jun. 2008
+
+Contact Information:
+====================
+Ajay Kumar
+Biometrics Research Laboratory
+Indian Institute of Technology Delhi
+New Delhi, India
+E-mail: ajaykr@ieee.org
+
--- bestDiff.py
+++ bestDiff.py
@@ -7,11 +7,24 @@ Created on Tue Mar 19 15:23:46 2019
 """
 import cv2
 import numpy as np
-def getIris(imageName,val):
-    #imageName= '01_L.bmp'
-    #val = 155
-    img1 = cv2.imread(imageName)
-    img = cv2.imread(imageName,0)
+from matplotlib import pyplot as plt
+import abel
+import skimage
+from PIL import Image 
+import glob
+from os import listdir
+
+
+import sys
+from scipy.misc import imread
+from scipy.linalg import norm
+from scipy import sum, average
+
+def getIris(img1,img,val):
+    
+    #img1 = cv2.imread(imageName)
+    #load the image in grayscale
+    #img = cv2.imread(imageName,0)   
     gray = img1
     ret, thresh = cv2.threshold(gray, val, 255, cv2.THRESH_BINARY)
     
@@ -23,14 +36,20 @@ def getIris(imageName,val):
     #cv2.imshow('detected ',gray)
     cimg=cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
     circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 10000, param1 = 50, param2 = 30, minRadius = 0, maxRadius = 0)
+    
+    xcentre = 0
+    ycentre = 0
+    radius = 0
+    
     for i in circles[0,:]:
-        i[2]=i[2]+4
+        i[2]=i[2] + 4
         # Draw on mask
         cv2.circle(mask,(i[0],i[1]),i[2],(255,255,255),thickness=-1)
-    
+        xcentre = i[0]
+        ycentre = i[1]
+        radius = i[2] + 4
     # Copy that image using that mask
-    masked_data = cv2.bitwise_and(img1, img1, mask=mask)
-    
+    masked_data = cv2.bitwise_and(img1, img1, mask = mask)
     # Apply Threshold
     _,thresh = cv2.threshold(mask,1,255,cv2.THRESH_BINARY)
     
@@ -39,35 +58,162 @@ def getIris(imageName,val):
     x,y,w,h = cv2.boundingRect(contours[0])
     
     # Crop masked_data
-    crop = masked_data[y:y+h,x:x+w]    
-    return crop
+    crop = masked_data[y:y+h,x:x+w]  
+    #plt.imshow(crop),plt.show()
+    return crop,radius,xcentre,ycentre
 
 def seeImagesviaName(nameimage):
     for i in nameimage:
         print(i)
-        val = cv2.imread(i,0)
-        cv2.imshow('detected Edge',val)
-    cv2.waitKey(0)    
+        val = cv2.imread(i)
+        plt.imshow(val),plt.show()
     
 def seeImages(images):
     for i in images:
-        cv2.imshow('detected Edge',i);
-    cv2.waitKey(0)
+        plt.imshow(i),plt.show()
 
 # Code to close Window
-    
-    
-#NAMES of all the images list
-ImageName = ['01_L.bmp']
-imags = []
-for i in range(0,len(ImageName)):
-    a = getIris(ImageName[i],155)
-    list.append(imags,a);
+##äPolar to Cartesian Convertor
 
-seeImagesviaName(ImageName)
-seeImages(imags);
+
+def toPolar(CartImage):
+    PolarImage, r_grid, theta_grid = abel.tools.polar.reproject_image_into_polar(CartImage)
+    
+    fig, axs = plt.subplots(1,2, figsize=(7,3.5))
+    axs[0].imshow(CartImage , aspect='auto', origin='lower')
+    axs[1].imshow(PolarImage, aspect='auto', origin='lower', 
+                  extent=(np.min(theta_grid), np.max(theta_grid), np.min(r_grid), np.max(r_grid)))
+    
+    axs[0].set_title('Cartesian')
+    axs[0].set_xlabel('x')
+    axs[0].set_ylabel('y')
+    
+    axs[1].set_title('Polar')
+    axs[1].set_xlabel('Theta')
+    axs[1].set_ylabel('r')
+    
+    plt.tight_layout()
+    plt.show()
+    return PolarImage
+###        
+def to_grayscale(arr):
+    "If arr is a color image (3D array), convert it to grayscale (2D array)."
+    if len(arr.shape) == 3:
+        return average(arr, -1)  # average over the last axis (color channels)
+    else:
+        return arr    
+    
+
+##loading the images from iitDatabase
+def loadImages(path):
+    # return array of images
+
+    imagesList = listdir(path)
+    loadedImages = []
+    k = 0
+    for image in imagesList:
+        img = Image.open(path + image)
+        loadedImages.append(img)
+        k = k+1;
+        if(k == 5):
+            break;
+    return loadedImages
+
+path = "IITDatabase/"
+##Loading ends
+
+#NAMES of all the images list
+inputimags = []
+imags = []
+radiuses = []
+xpoints = []
+ypoints = []
+# your images in an array
+for i in range(1,223):
+    x = "{0:0=3d}".format(i)+'/'
+    path1 = path + str(x)
+    list.append(inputimags,loadImages(path1))
+
+k = 0;idx = 0
+for i in inputimags:
+    for j in i:
+        img = np.asarray(j)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        a,r,x,y = getIris(img,gray,130)
+        k = k%5
+        if(k == 0):
+            idx = idx+1;
+        x1 = "{0:0=3d}".format(idx)
+        k = k+1;
+        name = 'Normalized/' + str(x1)+'_'+str(k) + '.bmp'
+        print(idx)
+        status = cv2.imwrite(name,a)
+        print(status)
+        list.append(imags,a);
+        list.append(radiuses,r);
+        list.append(xpoints,x);
+        list.append(ypoints,y);
+        
+for i in imags:
+    plt.imshow(i),plt.show()
+#print(xpoints,ypoints)
+#print(radiuses)
+#seeImages(imags)
+
+#normalizing the images .............................Not required
+k = 0;idx = 0
+for i in range(0,len(imags)):
+    k = k%5;
+    if(k == 0):
+        idx = idx+1;
+    k = k+1;
+    x = "{0:0=3d}".format(idx)
+    cl = imags[i]
+    im = to_grayscale(cl).astype(float)
+    normImg = toPolar(im)
+    name = 'Normalized/' + str(x)+'_'+str(k) + '.bmp'
+    print(name)
+    status = cv2.imwrite(name,normImg)
+    print(status)
+    
+#seeImagesviaName(ImageName)
+#seeImages(imags)
+#Remove orb features
+
+##observing the iris now
+#seeImages(imags);
+
+
 
 cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
